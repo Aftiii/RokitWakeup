a80250d5e5ff1b0d54703b07fdb46e35b9446231
Aftiii <robbie.aftab@gmail.com>
2021-12-04 17:13:31 +0000

    Come online before 10AM and set to a max vol

--- Rokit_WakeUp/Program.cs
+++ Rokit_WakeUp/Program.cs
@@ -16,7 +16,9 @@ namespace Rokit_WakeUp
         private static readonly int PLAY_TIME = 6;
         private static readonly string FILE_NAME = "C:\\Program Files (x86)\\RokitWakeUp\\MyFile.wav";
         private static readonly double HZ = 10;
-        private static double _currentVolume = 0;
+        private static double _currentVolume;
+        private static bool _leaveAtCurrentVolume = true;
+        private static TimeSpan _timeToLeaveAsScarlett = new TimeSpan(10, 0, 0);
 
         static void Main(string[] args)
         {
@@ -26,10 +28,54 @@ namespace Rokit_WakeUp
 
         static void WakeUp()
         {
-            SoundPlayer player = new SoundPlayer();
             CoreAudioController audioController = new CoreAudioController();
             CoreAudioDevice cad = audioController.DefaultPlaybackDevice;
-            if (!cad.FullName.ToLower().Contains("scarlett") && !cad.FullName.ToLower().Contains("speakers"))
+            SwitchToScarlett(audioController, cad);
+
+            cad = audioController.DefaultPlaybackDevice;
+            if (cad.FullName.Contains("Scarlett"))
+            {
+                DateTime currentDateTime = DateTime.Now;
+
+                if (currentDateTime.TimeOfDay < _timeToLeaveAsScarlett)
+                {
+                    _leaveAtCurrentVolume = false;
+                    PlayTone(cad);
+                }
+                else
+                {
+                    PlayTone(cad);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Default device is not Scarlett");
+                Thread.Sleep(5000);
+                Environment.Exit(0);
+            }
+        }
+
+        static void PlayTone(CoreAudioDevice cad)
+        {
+            _currentVolume = cad.Volume;
+            /*Console.WriteLine($"Current volume: {_currentVolume}");
+            Console.WriteLine($"Creating sound file with these parameters");
+            Console.WriteLine($"Play time:{PLAY_TIME}");
+            Console.WriteLine($"File name:{FILE_NAME}");
+            Console.WriteLine($"Hz:{HZ}");*/
+            CreateWAV.CreateWavFile(FILE_NAME, PLAY_TIME, HZ);
+            Console.WriteLine($"Setting volume to {MAX_VOLUME}");
+            cad.Volume = MAX_VOLUME;
+            Console.WriteLine($"Playing {FILE_NAME} for {PLAY_TIME} seconds");
+
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = FILE_NAME;
+            player.PlaySync();
+        }
+
+        static void SwitchToScarlett(CoreAudioController audioController, CoreAudioDevice cad)
+        {
+            if (!cad.FullName.ToLower().Contains("scarlett"))
             {
                 //switch audio device onto scarlett
                 List<CoreAudioDevice> cads = audioController
@@ -42,44 +88,25 @@ namespace Rokit_WakeUp
                     Thread.Sleep(5000);
                     System.Environment.Exit(0);
                 }
-                
+
                 audioController.SetDefaultDevice(cads[0]);
-
-            }
-
-            cad = audioController.DefaultPlaybackDevice;
-            if (cad.FullName.Contains("Scarlett"))
-            {
-                _currentVolume = cad.Volume;
-                Console.WriteLine($"Current volume: {_currentVolume}");
-                Console.WriteLine($"Creating sound file with these parameters");
-                Console.WriteLine($"Play time:{PLAY_TIME}");
-                Console.WriteLine($"File name:{FILE_NAME}");
-                Console.WriteLine($"Hz:{HZ}");
-                CreateWAV.CreateWavFile(FILE_NAME, PLAY_TIME, HZ);
-                Console.WriteLine($"Setting volume to {MAX_VOLUME}");
-                cad.Volume = MAX_VOLUME;
-                Console.WriteLine($"Playing {FILE_NAME}");
-                player.SoundLocation = FILE_NAME;
-                player.PlaySync();
-                Console.WriteLine("Done playing");
-
-                Console.WriteLine($"Setting volume back to {_currentVolume}");
-                cad.Volume = _currentVolume;
-                Console.WriteLine("Done");
-            }
-            else
-            {
-                Console.WriteLine("Default device is not Scarlett");
-                Thread.Sleep(5000);
-                System.Environment.Exit(0);
             }
         }
 
         static void CurrentDomain_ProcessExit(object sender, EventArgs e)
         {
             CoreAudioDevice cad = new CoreAudioController().DefaultPlaybackDevice;
-            cad.Volume = _currentVolume;
+            if (_leaveAtCurrentVolume)
+            {
+                Console.WriteLine($"Setting volume back to {_currentVolume}");
+                cad.Volume = _currentVolume;
+            }
+            else
+            {
+                Console.WriteLine($"Setting volume to max {MAX_VOLUME}");
+                cad.Volume = MAX_VOLUME;
+            }
+            Console.WriteLine($"Done");
         }
     }
 }
